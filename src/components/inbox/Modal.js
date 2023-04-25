e18aa85a7bb0d76eabd942187a39d34b9c542a9f
arunabhnewar <newardevil@gmail.com>
2023-04-25 19:14:26 +0600

    msg send handle & validation err

--- src/components/inbox/Modal.js
+++ src/components/inbox/Modal.js
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { conversationsApi } from "../../features/conversations/conversationsApi";
 import { useGetUserQuery } from "../../features/users/usersApi";
 import isValidEmail from "../../utilities/isValidEmail";
 import Error from "../ui/Error";
@@ -8,11 +10,41 @@ export default function Modal({ open, control }) {
     const [to, setTo] = useState("");
     const [message, setMessage] = useState("");
     const [userCheck, setUserCheck] = useState(false);
+    const [responseError, setResponseError] = useState("");
+    const [conversation, setConversation] = useState(undefined);
+
+    // dispatch
+    const dispatch = useDispatch()
+
+    // use selector
+    const { user: loggedInUser } = useSelector(state => state.auth) || {};
+    const { email: myEmail } = loggedInUser || {};
 
     // Hooks
     const { data: participant } = useGetUserQuery(to, {
         skip: !userCheck,
     });
+
+    // Use effect
+    useEffect(() => {
+
+        if (participant?.length > 0 && participant[0].email !== myEmail) {
+            // check conversation existence
+            dispatch(conversationsApi.endpoints.getConversation.initiate({
+                userEmail: myEmail,
+                participantEmail: to
+            })
+            ).unwrap()
+                .then(data => {
+                    console.log(data);
+                    setConversation(data);
+                })
+                .catch(err => {
+                    setResponseError("There was a problem!");
+                });
+        }
+
+    }, [participant, dispatch, myEmail, to]);
 
     // debounce handler
     const debounceHandler = (fn, delay) => {
@@ -36,6 +68,13 @@ export default function Modal({ open, control }) {
 
     // search handle with debounce
     const handleSearch = debounceHandler(doSearch, 500);
+
+    // msg submit handler
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Msg Send");
+    };
+
     return (
         open && (
             <>
@@ -47,7 +86,7 @@ export default function Modal({ open, control }) {
                     <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
                         Send message
                     </h2>
-                    <form className="mt-8 space-y-6">
+                    <form onSubmit={handleSubmit} className="mt-8 space-y-6">
                         <div className="rounded-md shadow-sm -space-y-px">
                             <div>
                                 <label htmlFor="to" className="sr-only">
@@ -86,6 +125,11 @@ export default function Modal({ open, control }) {
                             <button
                                 type="submit"
                                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
+                                disabled={
+                                    conversation === undefined ||
+                                    (participant?.length > 0 &&
+                                        participant[0].email === myEmail)
+                                }
                             >
                                 Send Message
                             </button>
@@ -94,6 +138,13 @@ export default function Modal({ open, control }) {
                         {participant?.length === 0 && (
                             <Error message="This user does not exist!" />
                         )}
+
+                        {participant?.length > 0 &&
+                            participant[0].email === myEmail && (
+                                <Error message="You can not send message to yourself!" />
+                            )}
+
+                        {responseError && <Error message={responseError} />}
                     </form>
                 </div>
             </>
